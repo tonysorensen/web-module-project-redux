2698aff6e5d1a46898d2882d23901af137cca88b
Tony Sorensen <tony48853@gmail.com>
2021-03-25 09:30:13 -0700

    state all hooked up

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import { connect } from "react-redux";
 const AddedFeature = props => {
+  console.log('props from AddedFeature', props)
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={()=>{}}className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state =>{
+  return{
+     car: state.car,
+  }
+}
+
+export default connect (mapStateToProps,{}) (AddedFeature);
